4b7be95e9fead70d9f6c5ff43eb5f0427159ef74
earthssu <entjq9502@khu.ac.kr>
2022-05-29 21:49:34 +0900

    jisu -/Lv3/heap

---
+++ 12W/jisu/디스크컨트롤러.py
@@ -0,0 +1,23 @@
+import heapq
+
+
+def solution(jobs):
+    answer, now, i = 0, 0, 0
+    start = -1
+    heap = []
+
+    while i <= len(jobs):
+        for job in jobs:
+            if start <= job[0] <= now:
+                heapq.heappush(heap, [job[1], job[0]])
+
+        if len(heap) > 0:
+            current = heapq.heappop(heap)
+            start = now
+            now += current[0]
+            answer += now - current[1]
+            i += 1
+        else:
+            now += 1
+
+    return int(answer / len(jobs))
